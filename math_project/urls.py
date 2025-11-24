b12ef7a79e91b0b4d880ac77fd489b06408ca7cb
Tohqa <tohqa@MacBook-Air-Tohqa.local>
2025-11-24 09:23:43 +0200

    Add URLs for GuessGame app

--- math_project/urls.py
+++ math_project/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from mathapp import views
+from mathapp import views as math_views
+from guessgame_app import views as guess_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.index, name='index'),
+    path('', math_views.index, name='index'),
 
-    path('quadratic/', views.quadratic_view, name='quadratic'),
+    path('quadratic/', math_views.quadratic_view, name='quadratic'),
+
+    path('guess/', guess_views.guess_view, name='guess'),
+    path('guess/submit/', guess_views.guess_submit, name='guess_submit'),
 ]
